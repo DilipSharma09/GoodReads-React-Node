fda07ebe739f3f1748ace22aff467c2375bff0ce
DilipSharma09 <49558595+DilipSharma09@users.noreply.github.com>
2019-04-20 17:05:15 +0530

    Update test.js

--- React/src/test.js
+++ React/src/test.js
@@ -11,7 +11,7 @@ var request = require('request');
 
         //Here we are expecting Total searched book data to be count 20
         //Will Pass
-        it('Correct Total Content', function(done) {
+        it('Correct Total Content Count', function(done) {
             request('http://localhost:8080/search?q="test"' , function(error, response, body) {
             var dd= JSON.parse(response.body).length;
                 expect(dd).to.equal(20);
@@ -21,7 +21,7 @@ var request = require('request');
 
          //Here we are expecting Total searched book data to be count 21
          // Will Fail
-        it('INCorrect Total Content', function(done) {
+        it('INCorrect Total Content Count', function(done) {
             request('http://localhost:8080/search?q="test"' , function(error, response, body) {
             var dd= JSON.parse(response.body).length;
                 expect(dd).to.equal(21);
@@ -29,6 +29,16 @@ var request = require('request');
             });
         });
 
+        //Here we are expecting bookid to be same what we searched
+        //Will Pass
+        it('Show Book detail Api test', function(done) {
+            request('http://localhost:8080/show?bookId=13326831' , function(error, response, body) {
+            var dd= JSON.parse(response.body).id._text;
+                expect(dd).to.equal('13326831');
+                done();
+            });
+        });
+        
 
     });
 
